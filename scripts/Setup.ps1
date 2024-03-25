b3611ce72127e96b93ac2e6871b8ced7f55ba995
Enes Sadık Özbek <es.ozbek@outlook.com>
2024-03-25 20:45:43 +0300

    Add nvm module to posh

--- scripts/Setup.ps1
+++ scripts/Setup.ps1
@@ -6,6 +6,7 @@ Install-Module -Name posh-git -Scope CurrentUser -Confirm
 Install-Module -Name PSReadLine -Scope CurrentUser -Confirm
 Install-Module -Name Get-ChildItemColor -Scope CurrentUser -Confirm
 Install-Module -Name Terminal-Icons -Scope CurrentUser -Confirm
+Install-Module -Name nvm -Scope CurrentUser -Confirm
 
 if (Test-Path $PROFILE) { Remove-Item $PROFILE }
 New-Item -Path $PROFILE -ItemType SymbolicLink -Value (Resolve-Path "..\Microsoft.PowerShell_profile.ps1").Path | Out-Null
