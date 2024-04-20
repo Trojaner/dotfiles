dda628718dde471f59748d86dde6d79c233a1723
Enes Sadık Özbek <es.ozbek@outlook.com>
2024-04-20 20:35:51 +0300

    posh: override prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -23,3 +23,18 @@ if (Test-Path alias:WGet) {
 
 $PSDefaultParameterValues["Out-File:Encoding"] = "utf8"
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+function prompt
+{
+  $loc = Get-Location
+
+  $prompt = & $GitPromptScriptBlock
+
+  $prompt += "$([char]27)]9;12$([char]7)"
+  if ($loc.Provider.Name -eq "FileSystem")
+  {
+    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+
+  $prompt
+}
