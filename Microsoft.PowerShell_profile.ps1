aa86c26e1423ef549e9c316e3ae4981dd69c02c8
Enes Sadık Özbek <es.ozbek@outlook.com>
2024-05-06 15:52:27 +0300

    Fix posh $prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -29,12 +29,12 @@ function prompt
   $loc = Get-Location
 
   $prompt = & $GitPromptScriptBlock
-
   $prompt += "$([char]27)]9;12$([char]7)"
-  if ($loc.Provider.Name -eq "FileSystem")
-  {
-    $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-  }
+
+  # if ($loc.Provider.Name -eq "FileSystem")
+  # {
+  #   $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  # }
 
   $prompt
 }
