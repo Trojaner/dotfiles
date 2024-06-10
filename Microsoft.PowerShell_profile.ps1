c0f6bd202e43c3ca3fdd6cdd68cca03fbed6871c
Enes Sadık Özbek <es.ozbek@outlook.com>
2024-06-10 22:09:23 +0300

    Update posh profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,13 @@
 # $POSH_CONFIG=(Join-Path $HOME "oh-my-posh.json")
 
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/agnoster.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jandedobbeleer.omp.json" | Invoke-Expression
 
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 
-Import-Module posh-git
 Import-Module PSReadLine
 Import-Module Get-ChildItemColor
 Import-Module Terminal-Icons
 
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
@@ -23,18 +22,3 @@ if (Test-Path alias:WGet) {
 
 $PSDefaultParameterValues["Out-File:Encoding"] = "utf8"
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
-
-function prompt
-{
-  $loc = Get-Location
-
-  $prompt = & $GitPromptScriptBlock
-  $prompt += "$([char]27)]9;12$([char]7)"
-
-  # if ($loc.Provider.Name -eq "FileSystem")
-  # {
-  #   $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-  # }
-
-  $prompt
-}
