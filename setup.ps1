58ccc5e463f93ba20d51135193e35eefb1c6c92d
Enes Sadık Özbek <1809172+Trojaner@users.noreply.github.com>
2026-02-08 18:37:29 +0300

    sync updates

--- setup.ps1
+++ setup.ps1
@@ -11,8 +11,8 @@ if (Test-Path  $PROFILE.CurrentUserAllHosts) {
 [System.Environment]::SetEnvironmentVariable('KUBE_EDITOR', 'code-insiders --wait', 'User')
 [System.Environment]::SetEnvironmentVariable('FZF_DEFAULT_OPTS', '--color=fg:-1,fg+:#ffffff,bg:-1,bg+:#3c4048 --color=hl:#5ea1ff,hl+:#5ef1ff,info:#ffbd5e,marker:#5eff6c --color=prompt:#ff5ef1,spinner:#bd5eff,pointer:#ff5ea0,header:#5eff6c --color=gutter:-1,border:#3c4048,scrollbar:#7b8496,label:#7b8496 --color=query:#ffffff --border="rounded" --border-label="" --preview-window="border-rounded" --height 40% --preview="bat -n --color=always {}"', 'User')
 
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Confirm
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Confirm
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 winget install --exact --source winget --id=JanDeDobbeleer.OhMyPosh --scope user --accept-source-agreements --silent
 winget install --exact --source winget --id=magic-wormhole.magic-wormhole --scope user --accept-source-agreements --silent
@@ -28,13 +28,13 @@ Enable-ExperimentalFeature PSFeedbackProvider
 Enable-ExperimentalFeature PSNativeWindowsTildeExpansion
 Enable-ExperimentalFeature PSSubsystemPluginModel
 
-Install-PSResource -Repository PSGallery -Name CompletionPredictor -Scope CurrentUser -Confirm
-Install-PSResource -Repository PSGallery -Name Get-ChildItemColor -Scope CurrentUser -Confirm
-Install-PSResource -Repository PSGallery -Name Microsoft.WinGet.Client -Scope CurrentUser -Confirm
-Install-PSResource -Repository PSGallery -Name Microsoft.WinGet.CommandNotFound -Scope CurrentUser -Confirm
-Install-PSResource -Repository PSGallery -Name posh-git -Scope CurrentUser -Confirm
-Install-PSResource -Repository PSGallery -Name PSReadLine -Scope CurrentUser -AllowPrerelease -Confirm
-Install-PSResource -Repository PSGallery -Name Terminal-Icons -Scope CurrentUser -Confirm
+Install-PSResource -Repository PSGallery -Name CompletionPredictor -Scope CurrentUser
+Install-PSResource -Repository PSGallery -Name Get-ChildItemColor -Scope CurrentUser
+Install-PSResource -Repository PSGallery -Name Microsoft.WinGet.Client -Scope CurrentUser
+Install-PSResource -Repository PSGallery -Name Microsoft.WinGet.CommandNotFound -Scope CurrentUser
+Install-PSResource -Repository PSGallery -Name posh-git -Scope CurrentUser
+Install-PSResource -Repository PSGallery -Name PSReadLine -Scope CurrentUser
+Install-PSResource -Repository PSGallery -Name Terminal-Icons -Scope CurrentUser
 
 Invoke-Expression "& {$(Invoke-RestMethod https://get.scoop.sh)} -RunAsAdmin"
 Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-aishell.ps1') }"
